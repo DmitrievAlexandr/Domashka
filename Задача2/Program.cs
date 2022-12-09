62e3789246f7f04618bbc79898be9b4f142d1ce7
Alexandr <milan.801813@gmail.com>
2022-12-09 23:53:31 +0300

    Домашняя работа

---
+++ Задача2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Вводим два чила: ");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+int numberB = Convert.ToInt32 (Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.Write("max: ");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write("max: ");
+    Console.Write(numberB);
+}
+
+
---
+++ Задача4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ведите три числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberС = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    if (numberA>numberС)
+    {
+        Console.Write("Max: ");
+        Console.Write(numberA);
+    }
+
+    else
+    {
+        if (numberС>numberB)
+        {
+            Console.Write("Max: ");
+            Console.Write(numberС);
+        }
+        else
+        {
+            Console.Write("Max: ");
+            Console.Write(numberB);
+        }
+    }
+    
+}
