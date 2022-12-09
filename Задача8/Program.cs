1908dbee4ee87e55a2d9c116ce9e30a56ef7b9dd
Alexandr <milan.801813@gmail.com>
2022-12-10 00:09:18 +0300

    Добавил гитигнор

---
+++ Задача8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число:");
+            try
+            {
+                var N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Четные числа из интервала 1 до " + N + ": ");
+                for (int i = 2; i <= N; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + ", ");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Значение не является числом");
+            }
